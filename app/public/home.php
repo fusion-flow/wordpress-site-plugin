5f8d06ddf3372fa33a39c4169de6302f26277d3a
Sanduni Ayesha <sanduniayesha.19@cse.mrt.ac.lk>
2023-12-17 00:17:52 +0530

    Add home.php

---
+++ app/public/home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resources</title>
+</head>
+
+<body>
+
+    <header>
+        <h1>Choose from below</h1>
+    </header>
+
+    <div class="Resources">
+        <div>
+            <a href="/com-support"> <p>Communication Support</p> </a></section>
+        </div>
+        <div>
+            <a href="/therapy"> <p>Therapy</p> </a>
+        </div>
+        <div>
+            <a href="/emotion-support"> <p>Emotions and Social Life</p> </a>
+        </div>
+        <div>
+            <a href="/life"> <p>Getting on with life</p> </a></section>
+        </div>
+        <div>
+            <a href="/tech-support"> <p>Help with technology</p> </a>
+        </div>
+    </div>
+</body>
+
+</html>
