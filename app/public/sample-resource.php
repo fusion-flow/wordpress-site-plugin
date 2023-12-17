1df2777d80355b6530860dfe7eda54c26f1086b6
AwesomeNipun <nipun1969@gmail.com>
2023-12-17 15:33:31 +0530

    Add sample resource page

---
+++ app/public/sample-resource.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample HTML Page</title>
+    <meta name="description" content="A simple HTML page with a description.">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        p {
+            color: #666;
+        }
+    </style>
+</head>
+
+<body>
+
+    <header>
+        <h1>Sample resource page</h1>
+    </header>
+
+    <section>
+        <p>This is a simple HTML page created for demonstration purposes. It includes a basic structure with a title, description, and some styling.</p>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Your Name. All rights reserved.</p>
+    </footer>
+
+</body>
+
+</html>
